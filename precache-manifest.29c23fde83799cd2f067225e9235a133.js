3ba58ffcccf28929f5dc801d70c158f670afda8a
Kelvin Almonte <kelvin.almonte95@gmail.com>
2020-02-11 19:52:26 -0500

    Updates

---
+++ precache-manifest.29c23fde83799cd2f067225e9235a133.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98d4063e8e05ffced6e3eac8f824df4e",
+    "url": "/timePass/index.html"
+  },
+  {
+    "revision": "3c10bb2fd27aa9043213",
+    "url": "/timePass/static/css/main.89dc02fe.chunk.css"
+  },
+  {
+    "revision": "a378dd5dade5719c0f59",
+    "url": "/timePass/static/js/2.0d488858.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/timePass/static/js/2.0d488858.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c10bb2fd27aa9043213",
+    "url": "/timePass/static/js/main.f6bc4f47.chunk.js"
+  },
+  {
+    "revision": "19798d5934808f35ea29",
+    "url": "/timePass/static/js/runtime-main.58d8cf19.js"
+  }
+]);
